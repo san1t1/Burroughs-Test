f1f43c326ab613f6eb95592ad025f57191084614
san1t1 <tim@san1t1.com>
2023-03-29 14:33:48 +0100

    Bootstrap a react app
    cra boostrao

---
+++ client/src/index.tsx
@@ -0,0 +1,19 @@
+
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './theme';
+
+const rootElement = document.getElementById('root') as Element;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+);
